97bb48bcae2f75967dbfc3a2adbf89f606d7ac74
Tofic <mehrazitawfik@gmail.com>
2020-01-01 17:04:30 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+            <title>MY VS projets</title>
+    </head>
+    <body>
+
+        <h3>You welcome to VS projets:</h3>
+        <<p>this th e first pro with git reposdjhvsd sdvk kj wecv welect Clone "Clone or download" on Github, 
+        copy the link.In Visual Studio Code, sect File -> Add Folder to Workspace -> Select the newly created directory.
+        Select Terminal Window.In the window, type:</p>
+    </body>
+</html>
